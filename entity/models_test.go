39623c4a755520e1b9e683824be00f89dba1e1ef
crhntr <hunter.christopher@icloud.com>
2018-03-31 21:45:30 -0700

    Cleaned up Models File

--- entity/models_test.go
+++ entity/models_test.go
@@ -5,32 +5,34 @@ import (
 	"github.com/mongodb/mongo-go-driver/bson/objectid"
 )
 
-const UserCol = "user"
+const (
+	UserCol = "user"
+	TeamCol = "team"
+	PostCol = "post"
+)
 
-type User struct {
-	entity.Entity `bson:",inline"`
-	Teams         []objectid.ObjectID `bson:"teams"`
-}
+type (
+	User struct {
+		entity.Entity `bson:",inline"`
+		Teams         []objectid.ObjectID `bson:"teams"`
+	}
+
+	Team struct {
+		entity.Entity `bson:",inline"`
+	}
+
+	Post struct {
+		entity.Entity `bson:",inline"`
+		N             int `bson:"n"`
+	}
+)
 
 func (this User) Ref() entity.EntityReference {
 	return entity.EntityReference{UserCol, this.ID}
 }
 
-const TeamCol = "team"
-
-type Team struct {
-	entity.Entity `bson:",inline"`
-}
-
 func (this Team) Ref() entity.EntityReference {
 	return entity.EntityReference{TeamCol, this.ID}
-}
-
-const PostCol = "post"
-
-type Post struct {
-	entity.Entity `bson:",inline"`
-	N             int `bson:"n"`
 }
 
 func (this Post) Ref() entity.EntityReference {
